9cef482b2257d74f98a5ec31d3ed6610b4fc587c
Wilson Cheung <wcheung1991@gmail.com>
2021-07-01 13:09:50 -0400

    added app.before_request()

--- app.py
+++ app.py
@@ -7,15 +7,17 @@ from flask import Flask, request, render_template, json
 
 
 app = Flask(__name__)
+@app.before_first_request
+def download_data():
+    global image_data
+    if not os.path.exists("./image_data/OMNO2d_HRM/{}.p".format('image_data')):
+        os.makedirs("./image_data/OMNO2d_HRM/", exist_ok=True)
+        wget.download('https://eo-dashboard-nasa.s3.amazonaws.com/image_data.p', out = "./image_data/OMNO2d_HRM/")
+    image_data = pickle.load(open( "./image_data/OMNO2d_HRM/{}.p".format('image_data'), "rb" ))
 
 @app.route('/', methods=['GET', 'POST'])
 def control_panel():
 #    print('request.form:', request.form)
-    if not os.path.exists("./image_data/OMNO2d_HRM/{}.p".format('image_data')):
-        os.makedirs("./image_data/OMNO2d_HRM/", exist_ok=True)
-        wget.download('https://eo-dashboard-nasa.s3.amazonaws.com/image_data.p', out = "./image_data/OMNO2d_HRM/")
-    if request.method == 'GET':
-        image_data = pickle.load(open( "./image_data/OMNO2d_HRM/{}.p".format('image_data'), "rb" ))
     if request.method == 'POST':
         volume = request.form.get('slide')
         convert = lambda x: tuple(sum(y) for y in zip((2016, 1), divmod(int(x), 12)))
